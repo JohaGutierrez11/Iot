7e213a3699b7fd7cad6a11a426cedc83c908eeb1
JohaGutierrez11 <joha.gutierrez.tabango@outlook.com>
2026-03-18 07:48:09 -0500

    g

--- WebTest.py
+++ WebTest.py
@@ -1,16 +1,20 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
+    #Si quiere que se muestre o no el navegador
     browser = p.chromium.launch(headless=False)
     page = browser.new_page()
 
+    #navega en la pagina
     page.goto("https://quotes.toscrape.com/js/")
+    #busca el selector
     page.wait_for_selector(".quote")
 
     quotes = page.query_selector_all(".quote")
 
     data = []
 
+    #muestra los textos y loa autores
     for q in quotes[:5]:
         text = q.query_selector(".text").inner_text()
         author = q.query_selector(".author").inner_text()
